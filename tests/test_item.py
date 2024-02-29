3c5c48c179669a493efdd5ffbc319b91fe4656a9
Alexander Kukin <KukinAU2008@mail.ru>
2024-02-29 22:48:17 +0400

    Добавлены тесты согласно hw2 : setter, getter для name; тест метода string_to_number; тест метода instantiate_from_csv.

--- tests/test_item.py
+++ tests/test_item.py
@@ -49,4 +49,41 @@ def test_fill_all(item, request):
     assert item in Item.all
 
 
+def test_instantiate_from_csv():
+    # это добавление в all должно затереться при вызове instantiate_from_csv, судя по логике описанной в main. hw2.
+    # Т.к. 5шт это только в csv файле
+    Item("наименование", 1, 2)
+    Item.instantiate_from_csv('src/items.csv')  # создание объектов из данных файла
+    assert len(Item.all) == 5  # в файле 5 записей с данными по товарам
+
+    with pytest.raises(FileNotFoundError):
+        Item.instantiate_from_csv('src/no_file.csv')
+
+
+def test_string_to_number():
+    assert Item.string_to_number('5') == 5
+    assert Item.string_to_number('5.0') == 5
+    assert Item.string_to_number('5.9') == 5
+    with pytest.raises(ValueError):
+         Item.string_to_number('error str num')
+
+
+def test_set_name():
+    """Проверка, что выставленную в сеттере строку при привышении 10 символов записываются только первый 10 символов"""
+
+    init_name = "Наименование заданное через инициализатор"
+    item = Item(init_name, 1, 2)
+    assert item.name == init_name
+
+    # наименование менее 10 символов, геттер выдаст полностью
+    new_name = "Машинка"
+    item.name = new_name
+    assert item.name == new_name
+
+    # наименования больше 10 символов
+    new_name = 'Машинка для стирки'
+    item.name = new_name
+    assert item.name == new_name[0: 10]
+
+    # Примечание. Если бы бросал  эксепшн в сеттере при привыешении 10 симвлов, то не получилось бы вернуть строку до 10 символов
 
