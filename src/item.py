82892166199d41c58c828021a544a6a5e678c33d
Alexander Kukin <KukinAU2008@mail.ru>
2024-02-29 22:50:22 +0400

    Добавлены методы и porperty согласно hw2 : setter, getter для name; метод string_to_number перевода строки в целое число без округления; метода instantiate_from_csv чтения из csv и создания на основе данные списка экземпляров item.

--- src/item.py
+++ src/item.py
@@ -1,3 +1,8 @@
+import csv
+import os.path
+from config import ROOT_PATH
+
+
 class Item:
     """
     Класс для представления товара в магазине.
@@ -13,10 +18,36 @@ class Item:
         :param price: Цена за единицу товара.
         :param quantity: Количество товара в магазине.
         """
-        self.name = name
+        self.__name = name
         self.price = price
         self.quantity = quantity
         Item.all.append(self)
+
+    @classmethod
+    def instantiate_from_csv(cls, file_path):
+        """Инициализирует экземпляры класса Item данными из файла src/items.csv"""
+        Item.all = []
+        file_path = os.path.join(ROOT_PATH, file_path)
+
+        items = []
+        with open(file_path, newline='', encoding="windows-1251") as csvfile:
+            reader = csv.DictReader(csvfile)
+            for row in reader:
+                name, price, quantity = row['name'], float(row['price']), int(row['quantity'])
+                items.append(Item(name, price, quantity))
+
+    @staticmethod
+    def string_to_number(str_num: str):
+        """Возвращает целую часть число из числа-строки"""
+        return int(float(str_num))
+
+    @property
+    def name(self):
+        return self.__name
+
+    @name.setter
+    def name(self, value):
+        self.__name = value[:10]
 
     def calculate_total_price(self) -> float:
         """
